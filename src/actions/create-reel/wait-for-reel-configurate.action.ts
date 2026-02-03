71fce2880cec7a7a3c33e18d11dff8f9884def40
Marcuth <thiago.playgames15@gmail.com>
2026-02-02 23:52:01 -0300

    evitando o código quebrar enquanto espera a resposta do instagram

--- src/actions/create-reel/wait-for-reel-configurate.action.ts
+++ src/actions/create-reel/wait-for-reel-configurate.action.ts
@@ -16,9 +16,9 @@ export const waitForReelConfigureAction: Action<PostData> = async ({ page }) => 
         if (json.media) {
             return {
                 caption: json.caption?.text ?? "",
-                id: json.media?.id,
-                pk: json.media?.pk,
-                code: json.media?.code,
+                id: json.media.id,
+                pk: json.media.pk,
+                code: json.media.code,
             } as PostData
         }
 
